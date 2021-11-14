c1885718bf5ce020fc6e26aaf908e8d347b5e920
zenlex <erichrkeil@gmail.com>
2021-11-14 13:53:25 -0600

    fix p5 init
    -move p5 import and main script links to head
    -include test draw code

--- turtle1/sketch.js
+++ turtle1/sketch.js
@@ -1,13 +1,18 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable require-jsdoc */
 const hello = document.createElement('h1').textContent='Hello World';
 document.querySelector('body').append(hello);
 function setup() {
-
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(204);
-  ellipse(50, 50, 80, 80);
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
 
 
