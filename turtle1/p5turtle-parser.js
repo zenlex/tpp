6fc6f4b9e3524f94345b9fa4671dfd7b146e3246
zenlex <erichrkeil@gmail.com>
2021-11-15 20:57:06 -0600

    Skeleton WIP
    - added form with text area input and handler
    - handler currently calls hard coded p5 sketch
    - handler needs to call parser

--- turtle1/p5turtle-parser.js
+++ turtle1/p5turtle-parser.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable require-jsdoc */
 /* eslint-disable no-unused-vars */
 /**
@@ -7,4 +8,18 @@
  * Using p5.js for the turtle graphics engine
  */
 
+/* -----------------------
+    TODO
+-----------------------*/
+/* whiteboard UI -
+    - user writes program in an html text area
+    - each instruction on newline
+    - space between command and argument
+    - # marks for comments.
+    - in html file, import parser script (which exports parse func)
+    - on submit for text area, submit text to parser
+    - parser reads text and returns code for new p5 program
+    - script in sketch contains function that takes a p5 setup and draw function as args, clears any existing p5 sketch and instantiates a new one with the passed functions
 
+    - need to write the parser as a p5 draw function that is called once for each line in the user input program
+    */
--- turtle1/sketch.js
+++ turtle1/sketch.js
@@ -2,17 +2,5 @@
 /* eslint-disable require-jsdoc */
 const hello = document.createElement('h1').textContent='Hello World';
 document.querySelector('body').append(hello);
-function setup() {
-  createCanvas(400, 400);
-}
-
-function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
-}
 
 
